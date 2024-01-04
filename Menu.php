25bee33a685479bf38d75aa77fc99f901f7b94c1
13fahmi <148124130+13fahmi@users.noreply.github.com>
2024-01-04 22:02:06 +0700

    Add files via upload

---
+++ Menu.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'pengguna', 
+			'label' => 'Pengguna', 
+			'icon' => '<i class="fa fa-user-secret "></i>'
+		),
+		
+		array(
+			'path' => 'barang', 
+			'label' => 'Barang', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'pelanggan', 
+			'label' => 'Pelanggan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'transaksi', 
+			'label' => 'Transaksi', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'transaksi_detail', 
+			'label' => 'Transaksi Detail', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'role_permissions', 
+			'label' => 'Role Permissions', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'roles', 
+			'label' => 'Roles', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
